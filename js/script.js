945a3f09a343a1692aa37480dd6ff35afe1b771a
Alisheroffalex <alisheroviskandar12@gmail.com>
2020-06-19 03:56:40 +0500

    19.06.2020

--- js/script.js
+++ js/script.js
@@ -1,15 +1,88 @@
 const ogConsole = console
 console = () => {}
 
-
-var myFullpage = new fullpage('#main', {
-    sectionsColor: ['#f2f2f2', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
+let myFullpage = new fullpage('#fullpage', {
     css3: true,
+    menu: '#nav',
     onLeave: function (origin, destination, direction) {
-        console.log(origin);
-        console.log(destination);
-        console.log(direction);
+        if(destination.isFirst) {
+            document.querySelector('nav').classList.remove('black');
+            document.querySelector('.lang-box').classList.remove('black');
+        } else {
+            document.querySelector('nav').classList.add('black');
+            document.querySelector('.lang-box').classList.add('black');
+        }
+
+        if(destination.isLast) {
+            document.querySelector('nav').style.display = 'none';
+            document.querySelector('.lang-box').style.display = 'none';
+        } else {
+            document.querySelector('nav').style.display = 'flex';
+            document.querySelector('.lang-box').style.display = 'flex';
+        }
     }
 });
-console = ogConsole
-console.log('hi')
+
+console = ogConsole;
+
+$('.projects-slider').slick({
+    dots: false,
+    infinite: false,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    prevArrow: '.prev-arrow',
+    nextArrow: '.next-arrow'
+});
+
+function initProjectInfo(index) {
+    let activeSlick = $('.slick-active');
+
+    $('#category').html(activeSlick.attr('data-category'));
+    $('#location').html(activeSlick.attr('data-location'));
+    $('.area-p').html(activeSlick.attr('data-area'));
+}
+
+$('.projects-slider').on('afterChange', function(event, slick, direction){
+    initProjectInfo(direction)    
+});
+
+function submitForm(form) {
+    $.ajax({
+        type: "POST",
+        url: 'index.html',
+        data: form.serialize(),
+        success: function() {
+
+        }
+    })
+}
+function validate(form)  {
+    let requiredFields = form.querySelectorAll('.required');
+    let errors = 0;
+    requiredFields.forEach(element => {
+        if (element.value.length == 0) {
+            element.parentElement.classList.add('warning');
+            errors++;
+        }
+    });
+
+    if(errors == 0) {
+        submitForm(form);
+    }
+
+    setTimeout(() => {
+        let warningFields = document.getElementsByClassName('input-group');
+        console.log(warningFields)
+        for (let i = 0; i < warningFields.length; i++) {
+            warningFields[i].classList.remove('warning');
+        }
+    }, 5000);
+}
+
+let form = document.getElementById('form');
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+    validate(form);
+})
+
